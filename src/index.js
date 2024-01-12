e033415307731269ba64f7a77394b97a0dde5243
ildikob09 <155365916+ildikob09@users.noreply.github.com>
2024-01-12 18:50:24 +0000

    added search function (using API)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
+function refreshWeather(response){
+    let temperatureElement = document.querySelector("#weather-app-temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+}
+
+
+function searchCity (city){
+let apiKey = "befe28ed9d48eac93fd074tafa91d7of";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`
+axios.get(apiUrl).then(refreshWeather);
+}
+
+
+
+
 function searchSubmit (event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+    
+    searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", searchSubmit);
+
+searchCity("Diss");
